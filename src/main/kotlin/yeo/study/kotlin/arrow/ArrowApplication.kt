68f40d6fe2773d9d43ac66e7f8016acec1181af4
yeo-chi <00www.black@gmail.com>
2024-08-17 13:57:20 +0900

    init

---
+++ src/main/kotlin/yeo/study/kotlin/arrow/ArrowApplication.kt
@@ -0,0 +1,11 @@
+package yeo.study.kotlin.arrow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArrowApplication
+
+fun main(args: Array<String>) {
+	runApplication<ArrowApplication>(*args)
+}
---
+++ src/test/kotlin/yeo/study/kotlin/arrow/ArrowApplicationTests.kt
@@ -0,0 +1,13 @@
+package yeo.study.kotlin.arrow
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArrowApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
